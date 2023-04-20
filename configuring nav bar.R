13ae6678b7a9ec0cba7d81a10aaaa92b2d9cfd7e
yehiel33 <yehiel33@gmail.com>
2023-04-20 16:37:39 +0300

    nav bar

---
+++ configuring nav bar.R
@@ -0,0 +1,19 @@
+# https://www.youtube.com/watch?v=6QYnzuqvbnw&list=PLpZT7JPM8_GZlqEssUJ6ABm0rblI1cBEW&index=5&ab_channel=JennySloane
+
+# for configring nav bar we will need to modify site.YML file:
+# adding two rows for each tab
+#   - text: "SQL"
+#    href: SQL.html
+
+
+# for drop down menu we will use by markdowd sintax:
+#- text: "SQL"
+#   menu:
+#     - text: Simple SQL 
+#       href: SIMSQL.html
+#     - text: Advanced SQL 
+#       href: ADVSQL.html
+
+# creating page:
+
+create_article("About_me")
